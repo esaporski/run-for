e60df42a33b20a97d90c5bf85bfe9cf9d49fdbf6
Eduardo Saporski <esaporski@protonmail.com>
2022-09-22 16:39:48 -0300

    refactor: format using shfmt

--- run-for.sh
+++ run-for.sh
@@ -4,100 +4,96 @@
 
 RUN_cmdname=${0##*/}
 
-usage()
-{
-    cat << USAGE >&2
+usage() {
+	cat <<USAGE >&2
 Usage:
     $RUN_cmdname [-t time] [-q] [-- command args]
     -t TIME | --time=TIME       Time (in seconds) to wait for script to exit
     -q | --quiet                Don't output any status messages
     -- COMMAND ARGS             Command with args to be executed
 USAGE
-    exit 1
+	exit 1
 }
 
-echoerr() { if [[ $RUN_QUIET -ne 1 ]]; then echo "$@" 1>&2; fi }
+echoerr() { if [[ $RUN_QUIET -ne 1 ]]; then echo "$@" 1>&2; fi; }
 
-run()
-{
-    echoerr "$RUN_cmdname: Starting command \"${RUN_CLI[@]}\" at $(date)"
-    RUN_start_ts=$(date +%s)
-    
-    ${RUN_CLI[@]}
-    CLI_RETURN=$?
+run() {
+	echoerr "$RUN_cmdname: Starting command \"${RUN_CLI[@]}\" at $(date)"
+	RUN_start_ts=$(date +%s)
 
-    if [[ $CLI_RETURN -ne 0 ]]; then
-        echoerr "$RUN_cmdname: Error executing command"
-        return $CLI_RETURN
-    fi
-    
-    echoerr "$RUN_cmdname: Command \"${RUN_CLI[@]}\" exited at $(date)"
-    RUN_stop_ts=$(date +%s)
+	${RUN_CLI[@]}
+	CLI_RETURN=$?
 
-    RUN_elapsed_time=$(($RUN_stop_ts - $RUN_start_ts))
-    echoerr "$RUN_cmdname: \"${RUN_CLI[@]}\" took $RUN_elapsed_time second(s) to run"
+	if [[ $CLI_RETURN -ne 0 ]]; then
+		echoerr "$RUN_cmdname: Error executing command"
+		return $CLI_RETURN
+	fi
 
-    SECONDS_TO_WAIT=$(($RUN_TIME-$RUN_elapsed_time))
+	echoerr "$RUN_cmdname: Command \"${RUN_CLI[@]}\" exited at $(date)"
+	RUN_stop_ts=$(date +%s)
 
-    if [[ $SECONDS_TO_WAIT -lt 1 ]]; then
-        echoerr "$RUN_cmdname: Command took longer than $RUN_TIME second(s)"
-        return 0
-    fi
+	RUN_elapsed_time=$(($RUN_stop_ts - $RUN_start_ts))
+	echoerr "$RUN_cmdname: \"${RUN_CLI[@]}\" took $RUN_elapsed_time second(s) to run"
 
-    echoerr "$RUN_cmdname: Waiting $SECONDS_TO_WAIT second(s) before exiting"
+	SECONDS_TO_WAIT=$(($RUN_TIME - $RUN_elapsed_time))
 
-    for second in `seq $SECONDS_TO_WAIT`
-    do
-        echoerr "$RUN_cmdname: $(($SECONDS_TO_WAIT - $second + 1)) second(s) to wait..."
-        sleep 1
-    done
-    return 0
+	if [[ $SECONDS_TO_WAIT -lt 1 ]]; then
+		echoerr "$RUN_cmdname: Command took longer than $RUN_TIME second(s)"
+		return 0
+	fi
+
+	echoerr "$RUN_cmdname: Waiting $SECONDS_TO_WAIT second(s) before exiting"
+
+	for second in $(seq $SECONDS_TO_WAIT); do
+		echoerr "$RUN_cmdname: $(($SECONDS_TO_WAIT - $second + 1)) second(s) to wait..."
+		sleep 1
+	done
+	return 0
 }
 
 # Process arguments
-while [[ $# -gt 0 ]]
-do
-    case "$1" in
-        -q | --quiet)
-          RUN_QUIET=1
-          shift 1
-          ;;
-        -t)
-          RUN_TIME="$2"
-          if [[ $RUN_TIME == "" ]]; then break; fi
-          shift 2
-          ;;
-        --time=*)
-          RUN_TIME="${1#*=}"
-          shift 1
-          ;;
-        --)
-          shift
-          RUN_CLI=("$@")
-          break
-          ;;
-        -h | --help)
-          usage
-          ;;
-        *)
-          echoerr "$RUN_cmdname: Unknown argument: $1"
-          usage
-          ;;
-    esac
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+	-q | --quiet)
+		RUN_QUIET=1
+		shift 1
+		;;
+	-t)
+		RUN_TIME="$2"
+		if [[ $RUN_TIME == "" ]]; then break; fi
+		shift 2
+		;;
+	--time=*)
+		RUN_TIME="${1#*=}"
+		shift 1
+		;;
+	--)
+		shift
+		RUN_CLI=("$@")
+		break
+		;;
+	-h | --help)
+		usage
+		;;
+	*)
+		echoerr "$RUN_cmdname: Unknown argument: $1"
+		usage
+		;;
+	esac
 done
 
 RUN_TIME=${RUN_TIME:-60}
 
 if [[ $RUN_TIME -lt 1 ]]; then
-    echoerr "$RUN_cmdname: Script must run for at least 1 second"
-    exit 1
+	echoerr "$RUN_cmdname: Script must run for at least 1 second"
+	exit 1
 fi
 
 if [[ $RUN_CLI != "" ]]; then
-    run
-    RUN_RETURN=$?
-    exit $RUN_RETURN
+	run
+	RUN_RETURN=$?
+	exit $RUN_RETURN
 else
-    echoerr "$RUN_cmdname: Must specify a command to run"
-    exit 1
+	echoerr "$RUN_cmdname: Must specify a command to run"
+	exit 1
 fi
